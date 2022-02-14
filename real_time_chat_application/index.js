df4679bba70890f4011a74b80ede3bd56b388ab4
shubh1998 <shubhamgupta7440@gmail.com>
2022-02-14 18:48:06 +0530

    feat: setup UI of application

--- real_time_chat_application/index.js
+++ real_time_chat_application/index.js
@@ -2,9 +2,16 @@ const socket = require("socket.io");
 const express = require("express");
 const app = express();
 const path = require("path")
-const staticContents = path.join(__dirname, '/public')
+const viewStaticContents = path.join(__dirname, '/public/views')
+const cssContents = path.join(__dirname, '/public/css')
+const jsContents = path.join(__dirname, '/public/js')
 
-app.use(express.static(staticContents))
+app.use(express.static(viewStaticContents))
+app.use(express.static(cssContents))
+app.use(express.static(jsContents))
+
+
+
 
 app.listen(4000, ()=>{
     console.log("Server starts listening at Port 4000")
